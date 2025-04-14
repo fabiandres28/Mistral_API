4eae0410fa5653505783cedbeeda74594e75aacb
fafigueroa <fafigueroa@indracompany.com>
2025-04-14 15:37:44 -0500

    final

--- api/upload_files.py
+++ api/upload_files.py
@@ -145,4 +145,4 @@ async def upload_files(files: List[UploadFile] = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
