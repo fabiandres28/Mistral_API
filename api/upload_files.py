091e6fad012546fd1ea4faf1121c4fc04f6d1506
fafigueroa <fafigueroa@indracompany.com>
2025-04-14 15:14:41 -0500

    Vercel

--- api/upload_files.py
+++ api/upload_files.py
@@ -18,7 +18,6 @@ api_key = os.getenv('api_key')
 client = Mistral(api_key=api_key)
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -146,4 +145,4 @@ async def upload_files(files: List[UploadFile] = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
